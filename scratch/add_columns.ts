acc951f9b2779a3a85b82a08b75bf54a4c36e226
ferilee <the.real.ferilee@gmail.com>
2026-05-13 11:33:24 +0700

    chore: add .gitignore and remove untracked files

--- scratch/add_columns.ts
+++
@@ -1,7 +0,0 @@
-import { Database } from 'bun:sqlite'
-const db = new Database('sqlite.db')
-db.run('ALTER TABLE users ADD COLUMN address TEXT')
-db.run('ALTER TABLE users ADD COLUMN contact TEXT')
-db.run('ALTER TABLE users ADD COLUMN profile_complete INTEGER DEFAULT 0')
-console.log('Columns added successfully')
-db.close()
--- scratch/check_db.ts
+++
@@ -1,5 +0,0 @@
-import { Database } from 'bun:sqlite';
-const db = new Database('sqlite.db');
-const users = db.query('SELECT * FROM users').all();
-console.log(JSON.stringify(users, null, 2));
-db.close();
